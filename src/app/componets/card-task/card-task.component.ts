327d113f96e5288b804b63f865ad9bae2e814154
GabrielCarrilloF <gabocarrillo.deve@gmail.com>
2024-08-14 00:08:45 -0500

    Done

--- src/app/componets/card-task/card-task.component.ts
+++ src/app/componets/card-task/card-task.component.ts
@@ -1,14 +1,51 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-card-task',
   templateUrl: './card-task.component.html',
   styleUrls: ['./card-task.component.scss'],
 })
-export class CardTaskComponent  {
+export class CardTaskComponent implements OnInit {
   @Input() datas: any;
 
-  constructor() { }
+  
+  constructor(private alertController: AlertController) { }
 
+  onCheckboxChange(event: any) {
+    const isChecked = event.detail.checked;
+    console.log('Checkbox state changed. Is checked:', isChecked);
+    // Aquí puedes realizar cualquier acción que desees según el estado del checkbox
+    this.presentAlert();
+  }
+
+  ngOnInit() {
+    
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Once the task is finished',
+      subHeader: '',
+      message: 'Happy your task is finished.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          },
+        },
+        {
+          text: 'Okay',
+          handler: () => {
+            console.log('Okay clicked');
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 
 }
